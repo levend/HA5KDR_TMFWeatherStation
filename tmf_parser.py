7556746f7aaefbb04094f41aead3176c30185f73
Balazs Gollner <gollner.balazs@gmail.com>
2021-08-03 22:33:23 +0200

    Adding config and button and relay support

--- tmf_parser.py
+++ tmf_parser.py
@@ -3,31 +3,27 @@ import serial
 import time
 import re
 from datetime import datetime
-
-arduino = serial.Serial(port='/dev/cu.usbserial-142230', baudrate=115200, timeout=.1)
-regex = r"(?P<id>\d{4})(?P<temp_0>(-|\+)\d{4})(?P<temp_05>(-|\+)\d{4})(?P<temp_2>(-|\+)\d{4})(?P<press>(-|\+)\d{4})(?P<hum>(-|\+)\d{4})"
-pattern = re.compile(regex);
+import os
+import configparser
+from gpiozero import LED
+from gpiozero import Button
 
 def read_tmf():
-    arduino.write("r\n".encode())
+    serial.write("r\n".encode())
     time.sleep(0.5)
-    data = arduino.readline()
+    data = serial.readline()
     return data
 
-while True:
-    input("Press ENTER to read the TMF") # Taking input from user
-    value = read_tmf().decode()
-    print("Raw data: {}".format(value)) # printing the value
-    test_str = "|;0036+0400-0410-0420+0900+0100*0000*0000|"
-    match = pattern.search(test_str)
-    
+
+def parse_tmf_response(match):
+
     device_id = match.group("id")
     temp_0_s = match.group("temp_0")
     temp_05_s = match.group("temp_05")
     temp_2_s = match.group("temp_2")
     pressure_s = match.group("press")
     humidity_s = match.group("hum")
-    
+
     temp_0 = int(temp_0_s) / 10.0
     temp_05 = int(temp_05_s) / 10.0
     temp_2 = int(temp_2_s) / 10.0
@@ -42,38 +38,77 @@ while True:
     print("A légnyomás {} milibar".format(pressure))
     print("Relatív páratartalom {} %".format(humidity))
 
-    #prepare the message file
-    with open("message_template.txt", "rt") as fp:
+    # prepare the message file
+    with open(settings['template_file_name'], "rt") as fp:
         template_content = fp.read()
         template_content = template_content.replace('{device_id}', device_id)
-        
+
         if temp_0 < 0:
-            template_content = template_content.replace('{temp_0}', "minusz {}".format(abs(temp_0)))
+            template_content = template_content.replace(
+                '{temp_0}', "minusz {}".format(abs(temp_0)))
         else:
-            template_content = template_content.replace('{temp_0}', str(temp_0))
+            template_content = template_content.replace(
+                '{temp_0}', str(temp_0))
 
         if temp_05 < 0:
-            template_content = template_content.replace('{temp_05}', "minusz {}".format(abs(temp_05)))
+            template_content = template_content.replace(
+                '{temp_05}', "minusz {}".format(abs(temp_05)))
         else:
-            template_content = template_content.replace('{temp_05}', str(temp_05))
+            template_content = template_content.replace(
+                '{temp_05}', str(temp_05))
 
         if temp_2 < 0:
-            template_content = template_content.replace('{temp_2}', "minusz {}".format(abs(temp_2)))
+            template_content = template_content.replace(
+                '{temp_2}', "minusz {}".format(abs(temp_2)))
         else:
-            template_content = template_content.replace('{temp_2}', str(temp_2))    
+            template_content = template_content.replace(
+                '{temp_2}', str(temp_2))
 
-        template_content = template_content.replace('{pressure}', str(pressure))
-        template_content = template_content.replace('{humidity}', str(humidity))
-        template_content = template_content.replace('{hour}', str(datetime.now().hour))
-        template_content = template_content.replace('{minute}', str(datetime.now().minute))
+        template_content = template_content.replace(
+            '{pressure}', str(pressure))
+        template_content = template_content.replace(
+            '{humidity}', str(humidity))
+        template_content = template_content.replace(
+            '{hour}', str(datetime.now().hour))
+        template_content = template_content.replace(
+            '{minute}', str(datetime.now().minute))
 
-        with open("message.txt","wt") as out_file:
+        with open(settings['message_file_name'], "wt") as out_file:
             out_file.write(template_content)
+            
+        relay.on()
+        time.sleep(0.2)
         
+        command_result = os.system(settings['speak_command'])
+        print('Command result {}'.format(command_result))
+        
+        relay.off()
 
 
+configparser = configparser.ConfigParser()
+configparser.read('config.ini')
+settings = configparser['DEFAULT']
 
+serial = serial.Serial(port=settings['serial_port'], baudrate=115200, timeout=.1)
+regex = r"(?P<id>\d{4})(?P<temp_0>(-|\+)\d{4})(?P<temp_05>(-|\+)\d{4})(?P<temp_2>(-|\+)\d{4})(?P<press>(-|\+)\d{4})(?P<hum>(-|\+)\d{4})"
+pattern = re.compile(regex)
+read_tmf() # initialize the port - no data will be returned for the first attempt
+button = Button(6, pull_up=False)
+relay = LED(5)
 
-
+while True:
+    if  button.is_pressed:
+        value = read_tmf().decode()
+        print("Raw data: {}".format(value))  # printing the value
+        test_str = "|;0036+0400-0410-0420+0900+0100*0000*0000|"
+        
+        #value = test_str
+        
+        match = pattern.search(value)
+        
+        if match:
+            parse_tmf_response(match)
+        else:
+            print("TMF data cannot be parsed - is the device connected/powered?")
 
 
